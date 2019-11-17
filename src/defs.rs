1dbaf11107291e6f43c1396a6876676b47c9e563
leo60228 <iakornfeld@gmail.com>
2019-11-17 17:31:57 -0500

    Skip fake loading screen

--- src/defs.rs
+++ src/defs.rs
@@ -1,7 +1,9 @@
 use detour::static_detour;
+use libc::c_void;
 
 static_detour! {
-    pub static PHYSFS_INIT: unsafe extern "C" fn(*mut libc::c_char) -> libc::c_int;
+    pub static HOOK_PHYSFS_INIT: unsafe extern "C" fn(*mut libc::c_char) -> libc::c_int;
+    pub static HOOK_PRELOADER_RENDER: unsafe extern "C" fn(*mut c_void, *mut c_void, *mut c_void);
 }
 
 dlsym! {
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,20 @@ mod defs;
 use crate::defs::*;
 use crate::helpers::*;
 use ctor::ctor;
+use libc::c_void;
 
 pub fn hook_physfs_init(argv0: *mut libc::c_char) -> libc::c_int {
     println!("PHYSFS_init({:?})", argv0);
     unsafe {
         PHYSFS_permitSymbolicLinks(1);
-        PHYSFS_INIT.call(argv0)
+        HOOK_PHYSFS_INIT.call(argv0)
+    }
+}
+
+pub fn hook_preloader_render(_dwgfx: *mut c_void, game: *mut c_void, _help: *mut c_void) {
+    let gamestate_ptr = game.wrapping_offset(112) as *mut libc::c_int;
+    unsafe {
+        *gamestate_ptr = 1;
     }
 }
 
@@ -28,13 +36,19 @@ fn init() {
     println!("{:?}", cmdline());
 
     let physfs_init = unsafe { get_symbol(b"PHYSFS_init") };
+    let preloader_render =
+        unsafe { get_symbol(b"_Z15preloaderrenderR8GraphicsR4GameR12UtilityClass") };
 
     dbg!(*physfs_init);
 
     unsafe {
-        PHYSFS_INIT
+        HOOK_PHYSFS_INIT
             .initialize(*physfs_init, hook_physfs_init)
             .unwrap();
-        PHYSFS_INIT.enable().unwrap();
+        HOOK_PHYSFS_INIT.enable().unwrap();
+        HOOK_PRELOADER_RENDER
+            .initialize(*preloader_render, hook_preloader_render)
+            .unwrap();
+        HOOK_PRELOADER_RENDER.enable().unwrap();
     }
 }
