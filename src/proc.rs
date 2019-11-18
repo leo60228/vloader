7517d55a714cccc334f1d02aa22ecebe5ce2429e
leo60228 <iakornfeld@gmail.com>
2019-11-18 18:48:44 -0500

    Don't use procfs on non-Linux

--- src/proc.rs
+++ src/proc.rs
@@ -1,14 +1,28 @@
-use std::fs;
 use std::path::PathBuf;
 
+#[cfg(target_os = "linux")]
+use std::fs;
+
+#[cfg(target_os = "linux")]
 pub fn exe() -> PathBuf {
     fs::read_link("/proc/self/exe").unwrap()
 }
 
+#[cfg(target_os = "linux")]
 pub fn cmdline() -> Vec<String> {
     fs::read_to_string("/proc/self/cmdline")
         .unwrap()
         .split('\0')
         .map(From::from)
         .collect()
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn cmdline() -> Vec<String> {
+    std::env::args().collect()
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn exe() -> PathBuf {
+    PathBuf::from(std::env::args().next().unwrap())
 }
